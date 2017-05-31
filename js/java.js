3e9cbbb31b553ffa5f1bfe8ae35e28f060f4ca41
Crakeus <camdude3124@gmail.com>
2017-05-31 16:50:30 -0700

    c

--- js/java.js
+++ js/java.js
@@ -1,9 +1,5 @@
 function main() {
-	$('.images').hide();
-	function fadeIn(obj) {
-      $(obj).fadeIn(1000);
-    }
-	$("#JTSnow").bind("load", function () { $(this).fadeIn(); });
+
 }
 
 $(document).ready(main);
