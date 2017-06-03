8b4fb9208197e7fb9c3a89a430b2805e05db7804
Crakeus <camdude3124@gmail.com>
2017-06-02 18:50:37 -0700

    testing JSON access

--- js/java.js
+++ js/java.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
    // jQuery methods go here...
+   $.get("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/Clàssic?api_key=cf1476c5-f500-4f4d-92aa-0929a43abad9", function( data ) {
+  $( ".result" ).html( data );
+  alert( "Load was performed." );
+   });
+   
 
 });
